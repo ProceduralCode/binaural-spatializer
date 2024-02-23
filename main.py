015ecc197dcca6a9cb4f32efb8e54d7c4661690c
ProceduralCode <37784785+ProceduralCode@users.noreply.github.com>
2024-02-22 23:59:11 -0800

    Synth testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import scipy.io.wavfile as wavfile
+from scipy.signal import iirfilter, sosfilt
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
@@ -26,6 +27,9 @@ def display_audio(audio):
 	plt.plot(audio)
 	plt.show()
 
+
+
+
 def apply_impulse_response(audio, impulse_response):
 	input_max = np.max(np.abs(audio))
 	if len(impulse_response.shape) == 2:
@@ -34,9 +38,47 @@ def apply_impulse_response(audio, impulse_response):
 		audio = np.convolve(audio, impulse_response)
 	return audio / np.max(np.abs(audio)) * input_max
 
-ir = read_audio('room_ir.wav')
-input = read_audio('my_voice.wav')
-output = apply_impulse_response(input, ir)
-play_audio(output)
-write_audio('output.wav', output)
+# ir = read_audio('room_ir.wav')
+# input = read_audio('my_voice.wav')
+# output = apply_impulse_response(input, ir)
+# play_audio(output)
+# write_audio('output.wav', output)
+
+
+
+
+
+
+
+
+def mix_audio(audio1, audio2, ratio):
+	return audio1 * ratio + audio2 * (1 - ratio)
+
+def pass_filter(audio, cutoff, order=4, type='highpass'):
+	sos = iirfilter(order, cutoff, btype=type, fs=sample_rate, output='sos')
+	if len(audio.shape) == 2:
+		return np.array([sosfilt(sos, audio[:, i]) for i in range(2)]).T
+	return sosfilt(sos, audio)
+
+
+
+def synth():
+	freq = 440
+	saw = np.linspace(0, 1, sample_rate) * freq % 2 - 1
+	out = pass_filter(saw, 800, type='lowpass')
+	return out
+
+def stereo_unison(freq, detune, spread, voices):
+	saws = []
+	for i in range(voices):
+		voice_freq = freq * (1 + detune * (i - (voices - 1) / 2))
+		pan = spread * (i - (voices - 1) / 2) / (voices - 1)
+		saw = np.linspace(0, 1, sample_rate) * voice_freq % 2 - 1
+		pan_saw = np.array([saw * (1 - pan), saw * (1 + pan)]).T
+		saws.append(pan_saw)
+	return np.mean(saws, axis=0)
+
+audio = stereo_unison(440, 0.01, 1, 3)
+audio = pass_filter(audio, 800, type='lowpass')
+play_audio(audio)
 
